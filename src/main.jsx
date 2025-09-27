fea02d3d269d96715733a5c1f8d148cf09f06f24
TrickyNoodle <sagarshinde2k20@gmail.com>
2025-09-27 19:49:34 +0530

    HashRouter!

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import App from "./App";
 import './index.css'
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 );
